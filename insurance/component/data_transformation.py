5af98697c23e2ce739265dfdf67e6f54e5eefe75
jjininu@gmail.com <jjininu@gmail.com>
2022-08-02 21:43:56 -0400

    update

--- insurance/component/data_transformation.py
+++ insurance/component/data_transformation.py
@@ -112,10 +112,10 @@ class DataTransformation:
 
 
             logging.info(f"Splitting input and target feature from training and testing dataframe.")
-            input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
+            input_feature_train_df = train_df.drop(columns="charges",axis=1)
             target_feature_train_df = train_df[target_column_name]
 
-            input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
+            input_feature_test_df = test_df.drop(columns="charges",axis=1)
             target_feature_test_df = test_df[target_column_name]
             
 
